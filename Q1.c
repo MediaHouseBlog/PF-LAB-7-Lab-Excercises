3e79476a84963196608ff977ae53b72da545ffca
Muhammad Siddique <mrsiddique19@gmail.com>
2025-10-13 11:27:10 +0500

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -14,6 +14,8 @@ main()
 	scanf("%d",&checkID);
 	
 	for(i = 0 ; i < 12 ; i++ ){
-		if(checkID == employeeID[i]) printf("Record Found In The Array\n"); 
+		if(checkID == employeeID[i]) printf("ID Found In The Array\n");
+		else printf("ID Not Found In The Array");
 	}
 }
+
