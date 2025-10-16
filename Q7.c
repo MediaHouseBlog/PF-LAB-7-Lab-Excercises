70a8fb9de7158214b8bfb69b26e566d152877acc
Muhammad Siddique <mrsiddique19@gmail.com>
2025-10-16 13:49:18 +0500

    Update Q7.c

--- Q7.c
+++ Q7.c
@@ -5,13 +5,19 @@ int main(){
 	char str[index];
 	int numbers[index];
 	printf("Enter the string ");
-	sanf("%s",str);
+	scanf("%s",str);
 	int i;
-	int j=1;
+	int j=0;
 	for(i = 0 ; i < index ; i++){
-		if(str[i] == '1' || str[i] == '2' ){
-			numbers[j] a= str[i];
+		if(str[i] == '0' || str[i] == '1' || str[i] == '2' || str[i] == '3' || str[i] == '4' || str[i] == '5' || str[i] == '6' || str[i] == '7' || str[i] == '8' || str[i] == '9'){
+			numbers[j] = (str[i] -'0');
 			j++;
 		}
 	}
+	int sum = 0;
+	for(i = 0 ; i < j ; i++){
+		printf("%d\n",numbers[i]);
+		sum += numbers[i];
+	}
+	printf("%d",sum);
 }
